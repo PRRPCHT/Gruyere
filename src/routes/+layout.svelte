547e8fcaee839996cf392c99d5148894bf7b9fd8
PRRPCHT <pierrepic@gmail.com>
2025-09-24 17:58:44 +0200

    Navbar layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,14 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import gruyere from '$lib/assets/gruyere_256px.webp';
+	import settings from '$lib/assets/settings.svg';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-<div class="mx-auto navbar max-w-6xl bg-base-100 shadow-sm">
+<div class="mx-auto navbar flex max-w-6xl flex-row justify-between bg-base-100 shadow-sm">
 	<div class="flex-none">
 		<label for="my-drawer-2" class="drawer-button btn btn-ghost lg:hidden"
 			><svg
@@ -50,22 +51,9 @@
 			</div>
 		</a>
 	</div>
-	<div class="flex-none">
-		<button class="btn btn-square btn-ghost">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block h-5 w-5 stroke-current"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-				></path>
-			</svg>
-		</button>
+	<div class="flex flex-none flex-row gap-2">
+		<button class="btn btn-ghost"> Settings </button>
+		<button class="btn btn-ghost"> Log out </button>
 	</div>
 </div>
 <main class="mx-auto max-w-6xl p-6">
