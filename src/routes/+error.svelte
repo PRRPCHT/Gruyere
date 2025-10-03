6cf5ba82c03ce0cddca074f3b242072cc9abe009
PRRPCHT <pierrepic@gmail.com>
2025-10-03 11:57:30 +0200

    Add default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<section class="mx-auto flex h-full w-full max-w-6xl flex-col justify-center gap-y-4">
+	<h1 class="mt-8 w-full text-center text-2xl">Something went wrong...</h1>
+	<div class="w-full text-center text-sm text-gray-500">
+		An error occurred. If the problem persists, please contact us on GitHub.
+	</div>
+	<div class="mt-2 w-full text-center text-sm text-gray-500">
+		<a href="/" class="btn btn-primary">Go to home</a>
+	</div>
+</section>
